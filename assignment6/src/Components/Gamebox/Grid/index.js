8c8de79d4db0a663f64012fbffdb5d6a99fca9a3
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-07-09 12:22:20 +0530

    game without syles

--- assignment6/src/Components/Gamebox/Grid/index.js
+++ assignment6/src/Components/Gamebox/Grid/index.js
@@ -8,39 +8,43 @@ class Grid extends Component{
         this.count=0; 
         this.state=({classes:this.props.className})  
     }
-    handleChange=(e)=>{
-        var b=this.props.value
-       
-        if(this.props.random.indexOf(Number(b))!==-1){
-            this.count+=1
-        this.setState(
-            {
-                classes:"clicked",
-               
-            }
-        )    
-            if(this.count===this.props.random.length){
-                console.log("hey")
-                this.props.Won()
-            }
+  componentDidMount() {
+             setTimeout(
+                () => this.setState(prevState => ({ classes:"block" })),
+                2000,
+            );
         }
+  componentWillReceiveProps(nextProps) {
+            if(nextProps.className!=this.props.classes){
+                this.setState({classes:nextProps.className})   
+            }
+            if(nextProps.className){
+                setTimeout(
+                    () => this.setState(prevState => ({ classes:"block" })),
+                    2000,
+                );
+            }
+        } 
+    handleChange=(e)=>{
+      
+        var b=this.props.value
+        console.log(b,this.props.random)
+        if(this.props.random.indexOf(Number(b))!==-1){
+            this.props.won()
+        this.setState({classes:"clicked"})     
+            }
         else{
-            this.setState(
-                {
-                    classes:"wclicked"
-                }
-            )
-              this.props.lost() 
-            }   
-    }
+            this.setState({classes:"wclicked"})
+              setTimeout(this.props.lost(),5000)
+              console.log(this.state.classes)
+            }          
+        }
    
     render(){
         return(
             <>
-          
             <div className={this.state.classes}  value={this.props.value}  onClick={this.handleChange}> </div>  
-            </>
-           
+            </>  
         );
     }
     }
--- assignment6/src/Components/Gamebox/index.js
+++ assignment6/src/Components/Gamebox/index.js
@@ -4,23 +4,25 @@ import Grid from './Grid'
 class Gamebox extends Component{
     constructor(props){
         super(props)
-        this.count=0;
         this.themenum=0
-        this.state={value:1,theme:"normal"}
+        this.wincount=0;
+        this.state={level:0,theme:"normal"}
     }
     Won=()=>{
+        this.wincount+=1
+        if(this.wincount===this.state.level+3){
+           this.wincount=0
         this.setState(
-            {value:this.state.value+1}
+            {level:this.state.level+1}
             )
-        
+        }   
     }
     Lost=()=>{
         this.setState(
-            {value:0}
-            )
+            {level:0}
+            )     
     }
-    generateRandom=()=>{
-        var n=this.state.value+3
+    generateRandom=(n)=>{
        var ranarr=[]
         while(ranarr.length < n){
             var r = Math.floor(Math.random()*(n*n));
@@ -33,41 +35,44 @@ class Gamebox extends Component{
         this.themenum+=1
         if(this.themenum%2===1){
         this.setState(
-            {theme:"night"}
+            {theme:"night",}
         )}
         else{
             this.setState(
-                {theme:"normal"}
+                {theme:"normal",}
             )}}
-    Createcols=(i,n,an)=>{
+    Createcols=(i,n,randomarr)=>{
          var j
-        var p=[]
+        var column=[]
+        
          for(j=0;j<n;j++){
-            if(an.indexOf(i)==-1){
-            p.push(<Grid value={i} className={"block"} random={an} level={this.state.value} won={this.Won}lost={this.Lost}/>)
+            console.log(randomarr,i)
+            if(randomarr.indexOf(i)==-1){
+            column.push(<Grid value={i} className={"block"} random={randomarr} level={this.state.level}  lost={this.Lost}/>)
             i+=n
             }
             else{
-                p.push(<Grid value={i}  className={"highlight"} random={an}  level={this.state.value} lost={this.lost}/>)
+                column.push(<Grid value={i}  className={"highlight"} random={randomarr}  level={this.state.level} won={this.Won} />)
                 i+=n  
             }
           }
-          return p
+          return column
          }
-    Creategrid=(n)=>{
-         var arr=[]
+    Creategrid=(n,randomarr)=>{
+         var gridarr=[]
             var i
-            var an=this.generateRandom()
+            var count=0
             for(i=0;i<n;i++){
-                arr.push(<div >{this.Createcols(this.count,n,an)}</div>)   
-                this.count+=1
+                gridarr.push(<div >{this.Createcols(count,n,randomarr)}</div>)   
+                count+=1
                 }
-            return arr
+            return gridarr
         }
     render(){
         return(
             <div class={this.state.theme}> 
-            <span class="level">level {this.state.value}</span>
+          
+            <span class="level">level {this.state.level}</span>
             <span class="theme">Theme mode:</span>
             <label class="switch">
                 <br/>
@@ -75,9 +80,11 @@ class Gamebox extends Component{
                  <span class="slider round"></span>
                     </label>
             <br />
+            
             <div class="grid">
-                {this.Creategrid(this.state.value+3)}
+              {this.Creategrid(this.state.level+3,this.generateRandom(this.state.level+3))}
                 </div>  
+               
             </div>
         );
     }
